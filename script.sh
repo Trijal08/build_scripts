cb5797271f3931535712a5ce090d6e7136c1ca86
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-11-06 01:01:39 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -25,9 +25,8 @@ echo "= Sync success ="
 echo "================"
 
 # Auto-sign build
-wget https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh
-chmod a+x create-signed-env.sh
-./create-signed-env.sh
+rm -rf vendor/lineage-priv/keys
+curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
 
 # Export
 export BUILD_USERNAME="GamerBoy1234294 • TenXBetter"
