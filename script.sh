ad4648b97228070f4b84a4033e9a33f43e8b2339
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-12-23 21:57:10 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-15-WIP-shusky .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-15-shusky .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
@@ -25,6 +25,7 @@ echo "= Sync success ="
 echo "================"
 
 # Directory setup
+
 cat > vendor/gms/common/Android.bp << EOF
 // Automatically generated file. DO NOT MODIFY
 //
