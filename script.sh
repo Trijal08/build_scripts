895e55bad890c967644381c012eefef31566748d
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-12-27 16:50:50 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,7 +30,7 @@ if [ ! -d vendor/lineage-priv ]; then
 fi
 
 # Export some info about us
-export BUILD_USERNAME="GamerBoy1234294 • Misty Fresh"
+export BUILD_USERNAME="Matthew Witherell • Flaring with Joy"
 export BUILD_HOSTNAME="crave"
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "==============="
