c2f866ac3553539799059e448f0b53b38910d443
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-12-28 02:16:11 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # Initialize the manifest
-repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/Trijal08/xperience_manifest.git -b xpe-19.0
+repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/CipherOS/android_manifest.git -b fifteen
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b XPerience-15.0-shusky .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Cipher_OS-15.0-shusky .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
