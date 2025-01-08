f0d6cbd541b8a9c6174f990665dd787e7d0ff1a1
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-01-07 23:00:58 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,7 +19,7 @@ echo "= Local manifests clone success ="
 echo "================================="
 
 # Sync repositories (now let that sync in)
-/opt/crave/resync.sh || curl -sSf https://raw.githubusercontent.com/Trijal08/build_scripts/refs/heads/sync_script/resync.sh | bash
+/opt/crave/resync.sh || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 echo "================"
 echo "= Sync success ="
 echo "================"
