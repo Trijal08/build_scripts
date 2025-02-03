010145de64134d883f4774a5de3bd5799d2b0865
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-02-03 17:04:05 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b crDroid-15.1-bonito .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-15.1-bonito .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
