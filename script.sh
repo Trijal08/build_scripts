6f25cb9de0eac0ef0177b7c7f546496e08f8432c
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-02-14 11:53:57 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # Initialize the manifest
-repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/Project-Mist-OS/manifest.git -b 15
+repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/OrionOS-Project/manifest.git -b vic
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-15.1-shusky .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b OrionOS-15.1-shusky .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
@@ -23,9 +23,6 @@ echo "================================="
 echo "================"
 echo "= Sync success ="
 echo "================"
-
-# Directory setup
-rm -rf vendor/addons/prebuilt/product/priv-app/BCR
 
 # Clone private signing keys
 if [ ! -d vendor/lineage-priv/keys ]; then
