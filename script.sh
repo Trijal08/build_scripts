dfe8fd921f7eedde9355369fe2ba3fb5de5078f6
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-03-13 15:48:15 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # Initialize the manifest
-repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/Project-Mist-OS/manifest.git -b 15
+repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/DroidX-UI/manifest.git -b 15_v2
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
@@ -45,9 +45,9 @@ echo "= Envsetup Done ="
 echo "================="
 
 # Delete any old builds
-mistify waffle user
+lunch droidx_waffle-bp1a-user
 make installclean -j$(nproc --all)
 
 # Lunch and build the ROM
-mistify waffle user
-mist sb
+lunch droidx_waffle-bp1a-user
+m bacon -j$(nproc --all)
