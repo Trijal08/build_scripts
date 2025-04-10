854c43e9bff16029464af8d4e3a07a1402f31a1f
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-04-09 21:37:46 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # Initialize the manifest
-repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/HorizonDroidLab/manifest.git -b fifteen
+repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/crdroidandroid/android.git -b 15.0
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b HorizonDroid-15.2-waffle .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b crDroid-15.2-waffle .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
