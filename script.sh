337bd4150b467628b605310c07aa24e1d88488e1
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-08-29 21:28:11 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # Initialize the manifest
-repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/Lunaris-AOSP/android.git -b 16
+repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/AxionAOSP/android.git -b lineage-23.0
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b LunarisOS-16.0-caimito .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-16.0-caimito .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
