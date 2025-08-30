fdbd06fdf29d9a81cfb56c68e6c269aedd235bc5
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-08-30 03:17:18 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # Initialize the manifest
-repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/RisingOS-Revived/android.git -b sixteen-aosp
+repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/RisingOS-Revived/android.git -b sixteen-los
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
