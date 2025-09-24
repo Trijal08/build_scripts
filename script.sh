72925fcaf637d3d6a87d84ef14ced363b6ec530e
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-09-23 21:35:14 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-16.0-lemonade .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-16.0-lemonadep .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
@@ -50,9 +50,9 @@ echo "= Envsetup Done ="
 echo "================="
 
 ## Delete any old builds
-mistify lemonade userdebug
+mistify lemonadep userdebug
 make installclean -j$(nproc --all)
 
 ## Lunch and build the ROM
-mistify lemonade userdebug
+mistify lemonadep userdebug
 mist b -j$(nproc --all)
