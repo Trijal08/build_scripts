972456b9889e23dd5668bfd3ef5b3e85946881a2
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-11-17 00:34:28 -0500

    Update branch in repo init command

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # Initialize the manifest
-repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/Project-Mist-OS/manifest.git -b bp2a
+repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/Project-Mist-OS/manifest.git -b 4.3
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
