d175de20db34806d27db8e81e90a593d6c52d4a2
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-05-17 10:27:28 -0400

    Fix branch name for local manifests and build commands

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-16.2-guacamole .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-16.2-guacamoleb .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
@@ -44,9 +44,9 @@ echo "= Envsetup Done ="
 echo "================="
 
 ## Delete any old builds
-mistify guacamole userdebug
+mistify guacamoleb userdebug
 make installclean -j$(nproc --all)
 
 ## Lunch and build the ROM
-mistify guacamole userdebug
+mistify guacamoleb userdebug
 mist b -j$(nproc --all)
