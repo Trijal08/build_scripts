a76f8a3ba341b6a17f1b4af9aa109eec863e19af
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-05-18 15:29:35 -0400

    Change local manifests branch and update build targets
    Updated the script to clone local manifests for the 'pantah' branch and modified build commands for Pixel 7 and Pixel 7 Pro.

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-16.2-caimito .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-16.2-pantah .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
@@ -40,23 +40,17 @@ echo "= Envsetup Done ="
 echo "================="
 
 ## Delete any old builds
-# Pixel 9 Pro XL
-mistify komodo userdebug
+# Pixel 7
+mistify pantah userdebug
 make installclean -j$(nproc --all)
-# Pixel 9 Pro
-mistify caiman userdebug
-make installclean -j$(nproc --all)
-# Pixel 9
-mistify tokay userdebug
+# Pixel 7 Pro
+mistify cheetah userdebug
 make installclean -j$(nproc --all)
 
 ## Lunch and build the ROM
-# Pixel 9 Pro XL
-mistify komodo userdebug
+# Pixel 7
+mistify pantah userdebug
 mist b -j$(nproc --all)
-# Pixel 9 Pro
-mistify caiman userdebug
-mist b -j$(nproc --all)
-# Pixel 9
-mistify tokay userdebug
+# Pixel 7 Pro
+mistify cheetah userdebug
 mist b -j$(nproc --all)
