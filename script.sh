d055df4b378d5030410cc294768d54ee84070853
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-07-27 13:13:15 -0400

    Update repository URLs and branches in script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # Initialize the manifest
-repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/Project-Mist-OS/manifest.git -b 16.2
+repo init --no-repo-verify --git-lfs --depth=1 -u https://github.com/Mist-OS-Staging/manifest.git -b 17
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Clone local manifests
-git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-16.2-caimito .repo/local_manifests
+git clone --depth=1 https://github.com/Trijal08/local_manifests.git -b Mist_OS-17.0-caimito .repo/local_manifests
 echo "================================="
 echo "= Local manifests clone success ="
 echo "================================="
